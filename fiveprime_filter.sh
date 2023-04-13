f5f351462e0687e9adda2be97a9cce6c42997276
dmnburrows <dominicburrows@hotmail.co.uk>
2023-04-13 04:24:16 +0000

    designing five prime end filtering scripts

---
+++ fiveprime_filter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Convert to ensembl format
+gtftk convert_ensembl -i rmsk.hg38.subsample.gtf -o rmsk.hg38.subsample.ensemble.gtf
+
+# calculate feature size
+gtftk feature_size -i rmsk.hg38.subsample.ensemble.gtf -o rmsk.hg38.subsample.featsize.gtf
+
+
+# filter by minimum feature size
+gtftk select_by_tx_size -i rmsk.hg38.subsample.featsize.gtf -o rmsk.hg38.subsample.filt.gtf -m 600
+
+
+#Turn into bed file
+
+
