40e176e3734d0967ed25e3c289fcc1eccf137c3f
dmnburrows <dominicburrows@hotmail.co.uk>
2023-05-04 17:19:51 +0000

    batch running STAR alignments for in PE data

--- workspace.sh
+++ workspace.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-
 str=$1
 
 #Define paths and variables
-inpath=$DATA3/te/rna/PE.fastq/merged/
-outpath=/cndd3/dburrows/DATA/te/rna/PE.fastq/trimmed
+inpath=/cndd3/dburrows/DATA/te/rna/PE.fastq/trimmed
+outpath=/cndd3/dburrows/DATA/te/rna/PE.bam/
 id_arr=($(find $inpath/ -maxdepth 1 -name "*Sample*$str*"))
 
 #Loop over each sample
 for i in ${id_arr[@]}
 do
-    echo $i
-    read_a=($(ls $i))
-    $CODE3/te_ageing/trim_galore.sh 8 3 20 0.1 $i/${read_a[0]} $i/${read_a[1]} $outpath/$(basename $i)
-    cp $CODE3/te_ageing/workspace.sh $outpath/$(basename $i)/log.workspace
-    cp $CODE3/te_ageing/trim_galore.sh $outpath/$(basename $i)/log.trim_galore
+    echo Running $i 
+    r1=$i/R1-merge_val_1.fq.gz
+    r2=$i/R2-merge_val_2.fq.gz
+    
+    mkdir $outpath/$(basename $i)
+    cd $outpath/$(basename $i)
 
+    STAR --genomeDir /cndd3/dburrows/DATA/te/gtf//annotations/gencode/STAR.gencode.v37 --readFilesIn $r1 $r2 --outSAMunmapped None --outFilterType BySJout --outSAMattributes All --outFilterMultimapNmax 100 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --sjdbScore 1 --readFilesCommand zcat --runThreadN 16 --genomeLoad LoadAndKeep --limitBAMsortRAM 10000000000 --outSAMtype BAM SortedByCoordinate --quantMode TranscriptomeSAM GeneCounts --winAnchorMultimapNmax 200 --outMultimapperOrder Random --outSAMmultNmax -1 
+    samtools index Aligned.sortedByCoord.out.bam
+    cp $CODE3/te_ageing/workspace.sh $outpath/$(basename $i)/log.workspace
 done
+
+
+
 
