67da529a3c3a3b058f7c25445eac7f1c53654103
dmnburrows <dominicburrows@hotmail.co.uk>
2023-05-22 22:51:02 +0000

    Computing evolutionary age

--- te_rna_f.py
+++ te_rna_f.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #===================================================
 def find_intersect(bam_remaining, flat_bed_pos, flat_bed_ind):
 #===================================================
@@ -18,7 +20,7 @@ def find_intersect(bam_remaining, flat_bed_pos, flat_bed_ind):
     ind: vector of pd row indeces of original bam file where reads have aligned
 
     """
-    import numpy as np
+    
 
     _int = np.intersect1d(bam_remaining['Start'].values, flat_bed_pos, return_indices=True)  #Find indeces (in the bam file of 5' aligned reads only) of reads whose tss overlaps with flattened bed vector
     umi = bam_remaining['UMI'].iloc[_int[1]].values #vector of UMIs that overlap with flattened bed
@@ -117,3 +119,94 @@ def five_prime_align(chr_bam, chr_bed,  count_df, bam_ind):
     curr_df['Count'] = counts_v 
     count_df = pd.concat([count_df, curr_df]) #Add them to the count matrix
     return(count_df, bam_ind)
+
+
+
+
+#==============================================================================
+def calculate_age(milli_div, subsitution_rate=2.2):
+#==============================================================================
+    
+    p = milli_div / 1000  # The milliDiv column in the `rmsk.txt` file.
+    p_part = (4 / 3) * p
+    jc_dist = -0.75 * (np.log(1 - p_part))
+    (jc_dist * 100) / (subsitution_rate * 2 * 100) * 1000
+    return jc_dist
+
+#=========================================
+def read_rmsk(filename: str):
+#=========================================
+
+    import gzip
+    from math import log
+    import pandas as pd
+
+    # read first line to check if it is a valid rmsk file
+    if filename.endswith(".gz"):
+        with gzip.open(filename) as f:
+            line = f.readline()
+    else:
+        with open(filename) as f:
+            line = f.readline()
+
+        assert (
+            line
+            == "   SW  perc perc perc  query      position in query           matching       repeat              position in  repeat\n"
+        ), "Not a valid rmsk file"
+
+    # setup converter functions
+    strand_conv = lambda x: "-" if x == "C" else "+"
+    coord_conv = lambda x: int(x.rstrip(")").lstrip("("))
+    perc_conv = lambda x: float(x) * 10
+
+    convs = {
+        "milliDiv": perc_conv,
+        "milliDel": perc_conv,
+        "milliIns": perc_conv,
+        "genoLeft": coord_conv,
+        "strand": strand_conv,
+        "repStart": coord_conv,
+        "repLeft": coord_conv,
+    }
+
+    # read the rmsk file
+    df = pd.read_csv(
+        filename,
+        skiprows=3,
+        delim_whitespace=True,
+        names=[
+            "swScore",
+            "milliDiv",
+            "milliDel",
+            "milliIns",
+            "genoName",
+            "genoStart",
+            "genoEnd",
+            "genoLeft",
+            "strand",
+            "repName",
+            "repClassFamily",
+            "repStart",
+            "repEnd",
+            "repLeft",
+            "id",
+        ],
+        converters=convs,
+    )
+
+    # split repClassFamily into repClass and repFamily on /
+    df[["repClass", "repFamily"]] = df["repClassFamily"].str.split("/", expand=True)
+    df.drop("repClassFamily", axis=1, inplace=True)
+
+    # calculate length of each repeat
+    df["length"] = df.apply(
+        lambda x: x["repEnd"] - x["repLeft"]
+        if x["strand"] == "-"
+        else x["repEnd"] - x["repStart"],
+        axis=1,
+    )
+
+    # calculate age of each repeat
+    df["age"] = df["milliDiv"].apply(calculate_age)
+
+    return df
