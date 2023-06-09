4cd4f109951537c9c25e473d5fd523ed10f537b6
dmnburrows <dominicburrows@hotmail.co.uk>
2023-06-09 23:07:57 +0000

    Analysing CZI data

---
+++ DESEQ_CZI.R
@@ -0,0 +1,32 @@
+library(DESeq2)
+library(tidyverse)
+
+setwd("/cndd3/dburrows/DATA/te/rna/CZI.counts/DESEQ/")
+coldata <- read.csv("ATEM_design.csv", row.names=1)
+
+#Loop over all the files in the current directory
+files <- list.files(pattern="*ATEM.csv")
+
+#Loop over each csv in files and run DESEQ
+for(i in files) {                                          
+  input <- i      
+    cnts <- read.csv(input, header=TRUE, check.names=FALSE, row.names=1)
+    dds <-DESeqDataSetFromMatrix(countData=cnts, 
+                                colData=coldata, 
+                                design=~Subject.Age+Subject.Sex) 
+    dds <- DESeq(dds)
+    res <- results(dds, alpha=0.1)
+    output <- paste0(str_sub(i, 1, -10), ".DESEQ_sex.csv")
+    print(output)
+    write.csv(as.data.frame(res), file=output)
+
+}
+
+
+
+
+
+
+
+
+
