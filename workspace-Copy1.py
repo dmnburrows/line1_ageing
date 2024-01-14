a727207036f3a989c4332868eab23a4920934650
dmnburrows <dominicburrows@hotmail.co.uk>
2024-01-13 16:00:57 -0800

    upstream lhs rna

--- workspace-Copy1.py
+++ workspace-Copy1.py
@@ -13,33 +13,36 @@ import multiprocessing
 import glob
 
 
-def process_directory(d):
-    run = f"""
-    echo $start
-    samtools view -b -F 0x40 {in_path}/{d}/Aligned.sortedByCoord.out.bam > {out_path}/{d}/Aligned.sortedByCoord.out.r2.bam
-    echo $samtools filter r2
-    samtools index {out_path}/{d}/Aligned.sortedByCoord.out.r2.bam
-    samtools view -b -F 0x100 {out_path}/{d}/Aligned.sortedByCoord.out.r2.bam > {out_path}/{d}/Aligned.sortedByCoord.out.r2.unq.bam
-    echo $
-    samtools index {out_path}/{d}/Aligned.sortedByCoord.out.r2.unq.bam
-    bedtools coverage -s -a /cndd3/dburrows/DATA/annotations/rmsk/l1hs_tss_bind.bed -b {out_path}/{d}/Aligned.sortedByCoord.out.r2.unq.bam > {out_path}/{d}/l1hs_unq.cov 
-    bedtools coverage -s -a /cndd3/dburrows/DATA/annotations/rmsk/l1hs_tss_bind.bed -b {out_path}/{d}/Aligned.sortedByCoord.out.r2.bam > {out_path}/{d}/l1hs.cov 
+def process_directory(s):
+    run = f""" 
+    samtools view {bam_path}/{s}/Aligned.sortedByCoord.out.bam | wc -l > {path}/{s}/total_reads.txt
+    samtools view -h -F 0x10 {bam_path}/{s}/Aligned.sortedByCoord.out.bam > {path}/{s}/plus.bam
+    samtools view -h -f 0x10 {bam_path}/{s}/Aligned.sortedByCoord.out.bam > {path}/{s}/minus.bam
+    samtools view -b -h -L /cndd3/dburrows/DATA/annotations/rmsk/rmsk.hg38.filt-5ptrim.plus.full.sort.bed {path}/{s}/plus.bam > {path}/{s}/plus.5pfilt_withsplice.bam
+    samtools index {path}/{s}/plus.5pfilt_withsplice.bam
+    samtools view -b -h -L /cndd3/dburrows/DATA/annotations/rmsk/rmsk.hg38.filt-5ptrim.minus.full.sort.bed {path}/{s}/minus.bam > {path}/{s}/minus.5pfilt_withsplice.bam
+    samtools index {path}/{s}/minus.5pfilt_withsplice.bam
+    
     """
-
     get_ipython().run_cell_magic('bash', '', run)
-    print('Done')
+    
+
+    print('Done' + s)
     
 
 #this section shows what default arguments will be run if just executing the script
 if __name__ == "__main__":
-    in_path = '/cndd3/dburrows/DATA/te/rna/PE.bam/'
-    out_path ='/cndd/dburrows/DATA/te/rna/PE.counts/l1hs_count_bind/'
-    dir_list = [os.path.basename(i) for i in glob.glob(in_path + '*Samp*')]
+    bam_path = '/cndd3/dburrows/DATA/te/rna/PE.bam/'
+    path = '/cndd/dburrows/DATA/te/rna/PE.counts/ATEM/'
+    samp_list = ['Sample_1134_GABA','Sample_1539_GABA', 'Sample_1648_GABA', 'Sample_198034-1435_GABA', 'Sample_4369_GABA',
+    'Sample_4414_GABA', 'Sample_4545_GABA', 'Sample_5077_GABA', 'Sample_5387_GABA', 'Sample_5401_GABA',
+    'Sample_5446_GABA', 'Sample_6007_GABA', 'Sample_HCT15HBNA032_GABA', 'Sample_HCT16HCQA020_GABA', 
+    'Sample_HCT16HECA028_GABA']
     
     # Create a pool of 10 worker processes
-    with multiprocessing.Pool(1) as pool:
+    with multiprocessing.Pool(10) as pool:
         # Map the process_directory function to each item in dir_list
-        pool.map(process_directory, dir_list)
+        pool.map(process_directory, samp_list)
 
     print("All tasks completed.")
     
--- workspace.py
+++ workspace.py
@@ -11,30 +11,24 @@ import numpy as np
 import multiprocessing
 import glob
 
+import te_rna_f as ter
+sys.path.insert(1, '/cndd3/dburrows/CODE/admin_tools/')
+from admin_tools import admin_functions as adm
 
-def process_directory(d):
-    run = f"""
-    samtools view -b -F 0x100 {out_path}/{d}/L1HS_all.bam > {out_path}/{d}/L1HS_all.unq.bam
-    samtools index {out_path}/{d}/L1HS_all.unq.bam
-    bedtools intersect -s -u -a {in_path}/{d}/Aligned.sortedByCoord.out.r2.bam -b /cndd3/dburrows/DATA/annotations/rmsk/rmsk.hg38.filt-5ptrim.merge.L1HS_all.canon.extended.bed > {out_path}/{d}/L1HS_all.extended.bam
-    samtools index {out_path}/{d}/L1HS_all.extended.bam
-    samtools view -b -F 0x100 {out_path}/{d}/L1HS_all.extended.bam > {out_path}/{d}/L1HS_all.extended.unq.bam
-    samtools index {out_path}/{d}/L1HS_all.extended.unq.bam
-
-    """
-    get_ipython().run_cell_magic('bash', '', run)
-
+def multimap(d):
+    curr_ = ter.multimap_stats(d+'/L1HS_all.bam')
+    curr_.to_csv(d+'/multimap.csv', index=False)
+    print('Done ' + d)
 
 #this section shows what default arguments will be run if just executing the script
 if __name__ == "__main__":
-    in_path = '/cndd3/dburrows/DATA/te/rna/PE.bam/'
-    out_path='/cndd/dburrows/DATA/te/rna/PE.counts/l1hs/'
-    dir_list = [os.path.basename(i) for i in glob.glob(in_path + '*Samp*')]
+    path='/cndd/dburrows/DATA/te/rna/PE.counts/l1hs/'
+    dir_list = glob.glob(path + '*Samp*')
     
     # Create a pool of 10 worker processes
     with multiprocessing.Pool(10) as pool:
         # Map the process_directory function to each item in dir_list
-        pool.map(process_directory, dir_list)
+        pool.map(multimap, dir_list)
 
     print("All tasks completed.")
     
